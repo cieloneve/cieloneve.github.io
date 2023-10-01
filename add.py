39509bf0c122176b7644aecf66b5b3ccf89e6b5b
cieloneve <antidust1069@gmail.com>
2023-10-01 14:18:19 +0800

    add event 108

--- add.py
+++ add.py
@@ -10,11 +10,13 @@ for x in range(0,num):
 
 for x in range(0,num):
     image_url=f"https://assets.pjsek.ai/file/pjsekai-assets/startapp/character/member_cutout/res{res[x]}_no{no[x]}/after_training/deck_s.png"
+    image_url=f"https://storage.sekai.best/sekai-assets/character/member_cutout/res{res[x]}_no{no[x]}_rip/after_training.png"
     print(image_url)
     img_data = requests.get(image_url).content
     with open(f'./small/res{res[x]}/no{no[x]}.png', 'wb') as handler:
         handler.write(img_data)
     image_url=f"https://assets.pjsek.ai/file/pjsekai-assets/startapp/character/member_cutout/res{res[x]}_no{no[x]}/normal/deck_s.png"
+    image_url=f"https://storage.sekai.best/sekai-assets/character/member_cutout/res{res[x]}_no{no[x]}_rip/normal.png"
     img_data = requests.get(image_url).content
     with open(f'./small/res{res[x]}/no{no[x]}_normal.png', 'wb') as handler:
         handler.write(img_data)
